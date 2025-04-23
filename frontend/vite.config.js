a91d629cdc37c745159d623f7dce248fb7eff5f3
ravigupta <rg187888@gmail.com>
2025-04-23 12:23:56 +0530

    vite.config chaged

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // default for Vite, but good to be explicit
-  },
-  server: {
-    port: 5173, // optional, for local dev
-  }
 })
